8d1f82e15a2ba83d225e79774057353f0ce49284
Jesus Pedro Bapolo <jesusestudante@gmail.com>
2025-10-16 20:38:57 +0100

    commit

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import { redirect } from "next/navigation";
 
 export default function Home() {
   return (
-    <main className="bg-[#1F1E28] flex flex-col gap-4 text-zinc-200 p-8 h-[100vh]">
+    <main className="bg-[#1F1E28] flex flex-col gap-4 text-zinc-200 p-8 md:h-[100vh]">
       <ul>
         <li>Essa é uma página sinistra onde vão aparecer todos que estiverem cadastrados com o @Voldemort</li>
         <li>Essa é uma página sinistra onde vão aparecer todos que estiverem cadastrados com o @Voldemort</li>
